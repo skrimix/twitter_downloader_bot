0b9e5483db1bd58c2e8901161bd5ab45f4b7dc43
skrimix <skrimix@posteo.net>
2021-11-20 16:24:07 +0500

    Use separate config for dev

---
+++ config_dev.py
@@ -0,0 +1,3 @@
+BOT_TOKEN = "${{ secrets.bot_token }}"  # Your telegram bot token
+DEVELOPER_ID = int("${{ secrets.developer_id }}")  # Your telegram id (used for error reporting and private mode)
+IS_BOT_PRIVATE = True  # Change this to True to make bot private
