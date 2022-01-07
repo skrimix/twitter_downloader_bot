a24e4fb9bda97b77d765f7da8089f72466964f43
skrimix <28156185+skrimix@users.noreply.github.com>
2022-01-07 23:58:40 +0500

    Store production config in a separate file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "${{ secrets.bot_token }}"  # Your telegram bot token
-DEVELOPER_ID = int("${{ secrets.developer_id }}")  # Your telegram id (used for error reporting and private mode)
+BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # Your telegram bot token
+DEVELOPER_ID = 1234567890  # Your telegram id (used for error reporting and private mode)
 IS_BOT_PRIVATE = False  # Change this to True to make bot private
--- config_prod.py
+++ config_prod.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "${{ secrets.bot_token }}"
 DEVELOPER_ID = int("${{ secrets.developer_id }}")
-IS_BOT_PRIVATE = True
+IS_BOT_PRIVATE = False
